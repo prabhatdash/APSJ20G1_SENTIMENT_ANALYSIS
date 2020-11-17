ef7e6578bb1879979800bf6f8ffe77ae3885e48f
chiranjeetborah <3812@apsjorhat.org>
2020-11-17 14:03:59 +0700

    Mysql_registration_system

---
+++ project_file.py
@@ -0,0 +1,53 @@
+import mysql.connector as db
+import smtplib
+import random
+mydb=db.connect(host="localhost",passwd="mysql",user="root",database="apsjorhat")
+mycursor=mydb.cursor()
+
+
+def user_auth():
+    email = input("Enter your email to login")
+    fetch_query = "select * from reg_users;"
+    mycursor.execute(fetch_query)
+    for i in mycursor:
+        if i[1] == email:
+            print("Login Successful !")
+            count = 1
+            break
+    if count == 0:
+        print("Login Error !")
+
+print("******************* Enter 1 to LOGIN or 2 to REGISTER for new users *******************")
+inp=int(input())
+count=0
+if inp==1:
+   user_auth()
+
+elif inp==2:
+    name = input("Enter your name")
+    email = input("Enter your email")
+    otp = str(random.randint(100000,1000000))
+
+    SUBJECT = "OTP for Login"
+    TEXT = "HEY "+name+'!' "\r\n""\r\n" 'Your OTP for login is: ' + otp+ "\r\n""\r\n" 'Please enter the otp for further verification'"\r\n""\r\n"'Thank you !'
+    s = smtplib.SMTP('smtp.gmail.com', 587)
+    s.starttls()
+    s.login('group1@apsjorhat.org', 'apsj#12345678')
+    message = 'Subject: {}\n\n{}'.format(SUBJECT, TEXT)
+    s.sendmail('group1@apsjorhat.org', email, message)
+    s.quit()
+    print("An OTP was sent to the entered email address")
+
+
+    answer=int(input("Please enter the otp for verification"))
+    if answer == int(otp) :
+        query = "insert into reg_users values('{}','{}');".format(name, email)
+        mycursor.execute(query)
+        mydb.commit()
+        print("You have been successfully registered. Now please follow the instructions to succesfully Login ")
+        user_auth()
+
+    else:
+        print("Sorry,the otp you entered is incorrect")
+elif inp != 1 and inp !=2:
+    print("******************* Please try again ! *******************")
